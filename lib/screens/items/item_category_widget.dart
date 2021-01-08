f6b0b379151b6da4ca66c1a530a5499bd43aa19b
Alexander Bang <>
2021-01-08 13:36:47 +0100

    changes

---
+++ lib/screens/items/item_category_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../models/categories.dart';
+
+class ItemCategoryItem extends StatelessWidget {
+  final ItemCategory category;
+  final bool selectCategory;
+  final bool selected;
+  final Function toggle;
+  final List list;
+
+  ItemCategoryItem(
+    this.category,
+    this.selectCategory,
+    this.selected,
+    this.toggle,
+    this.list,
+  );
+
+  void onTap(BuildContext context) {
+    if (selectCategory == true) {
+      Navigator.of(context).pop(category.title);
+    } else {
+      Navigator.of(context).pushNamed('/item', arguments: category.title);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool isSelected = list.contains(category);
+
+    return Card(
+      child: ListTile(
+        title: Text(category.title),
+        onTap: () {
+          selected ? toggle(category) : onTap(context);
+        },
+        onLongPress: () => toggle(category),
+        trailing: Offstage(
+          offstage: !selected,
+          child: Checkbox(
+            value: isSelected,
+            onChanged: (_) {},
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/items/items_category_screen.dart
+++ lib/screens/items/items_category_screen.dart
@@ -4,12 +4,36 @@ import 'package:provider/provider.dart';
 import '../../l10n/app_localizations.dart';
 
 import '../../providers/item_categories.dart';
-import '../../providers/items.dart';
-
+import '../../models/categories.dart';
 import '../../widgets/popup_add_category.dart';
+import './item_category_widget.dart';
 
-class ItemsCategoriesScreen extends StatelessWidget {
+class ItemsCategoriesScreen extends StatefulWidget {
   static const routeName = '/';
+
+  @override
+  _ItemsCategoriesScreenState createState() => _ItemsCategoriesScreenState();
+}
+
+class _ItemsCategoriesScreenState extends State<ItemsCategoriesScreen> {
+  bool _selectable = false;
+  List _selected = [];
+
+  void _toggleSelection(ItemCategory category) {
+    setState(() {
+      if (!_selectable) {
+        _selectable = true;
+      }
+      if (_selected.contains(category)) {
+        _selected.remove(category);
+        if (_selected.isEmpty) {
+          _selectable = false;
+        }
+      } else {
+        _selected.add(category);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,42 +46,15 @@ class ItemsCategoriesScreen extends StatelessWidget {
                 child: Text('Add Category First'),
               )
             : ListView.builder(
-                itemBuilder: (context, i) => ChangeNotifierProvider(
-                  create: (context) => Items(),
-                  child: ItemCategoryItem(
-                      data.categories[i].title, _selectCategory),
+                itemBuilder: (context, i) => ItemCategoryItem(
+                  data.categories[i],
+                  _selectCategory,
+                  _selectable,
+                  _toggleSelection,
+                  _selected,
                 ),
                 itemCount: data.categories.length,
               ),
-      ),
-    );
-  }
-}
-
-// Single Category Item
-class ItemCategoryItem extends StatelessWidget {
-  final String title;
-  final bool select;
-
-  ItemCategoryItem(this.title, this.select);
-
-  void onTap(BuildContext context) {
-    if (select == true) {
-      Navigator.of(context).pop(title);
-    } else {
-      Navigator.of(context).pushNamed('/item', arguments: title);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: InkWell(
-        onTap: () => onTap(context),
-        onLongPress: () {},
-        child: ListTile(
-          title: Text(title),
-        ),
       ),
     );
   }
