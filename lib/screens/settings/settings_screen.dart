3bbb252e4ab8ad2e09f234a21d063171c97332fa
Alexander Bang <>
2021-01-28 21:06:14 +0100

    changes to settings screen

--- lib/screens/settings/settings_screen.dart
+++ lib/screens/settings/settings_screen.dart
@@ -4,76 +4,85 @@ import '../../l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../../widgets/custom_app_bar.dart';
 
-class SettingsScreen extends StatefulWidget {
+class SettingsScreen extends StatelessWidget {
   static const routeName = '/settings';
 
-  @override
-  _SettingsScreenState createState() => _SettingsScreenState();
-}
-
-class _SettingsScreenState extends State<SettingsScreen> {
-  Locale language;
-
-  @override
-  void didChangeDependencies() {
-    SharedPreferences.getInstance().then((preferences) {
-      String string = preferences.getString('language');
-      setState(() {
-        language = Locale(string);
-      });
-    });
-    super.didChangeDependencies();
+  Future<Locale> _getLocale() async {
+    return await SharedPreferences.getInstance()
+        .then((preferences) => Locale(preferences.getString('language')));
   }
 
-  selectDialog(BuildContext context) {
+  _selectLanguageDialog(BuildContext context) {
     void _select(Locale value) async {
-      language = value;
       SharedPreferences preferences = await SharedPreferences.getInstance();
       preferences.setString('language', value.languageCode);
       MyApp.setLocale(context, value);
       Navigator.of(context).pop();
     }
 
-    showDialog(
-      context: context,
-      child: StatefulBuilder(
-        builder: (context, setState) => SimpleDialog(
-          title: Text(AppLocalizations.of(context).selectLang),
-          children: [
-            RadioListTile(
-              value: Locale('en'),
-              groupValue: language,
-              onChanged: (value) {
-                setState(() {
-                  _select(value);
-                });
-              },
-              title: Text('English'),
+    _getLocale().then((language) => showDialog(
+          context: context,
+          child: StatefulBuilder(
+            builder: (context, setState) => SimpleDialog(
+              title: Text(AppLocalizations.of(context).selectLang),
+              children: [
+                RadioListTile(
+                  value: Locale('en'),
+                  groupValue: language,
+                  onChanged: (value) => setState(() {
+                    _select(value);
+                  }),
+                  title: Text('English'),
+                ),
+                RadioListTile(
+                  value: Locale('de'),
+                  groupValue: language,
+                  onChanged: (value) => setState(() {
+                    _select(value);
+                  }),
+                  title: Text('Deutsch'),
+                ),
+                RadioListTile(
+                  value: Locale('da'),
+                  groupValue: language,
+                  onChanged: (value) => setState(() {
+                    _select(value);
+                  }),
+                  title: Text('Dansk'),
+                ),
+              ],
             ),
-            RadioListTile(
-              value: Locale('de'),
-              groupValue: language,
-              onChanged: (value) {
-                setState(() {
-                  _select(value);
-                });
-              },
-              title: Text('Deutsch'),
+          ),
+        ));
+  }
+
+  Future<int> _getInWashTime() async {
+    return await SharedPreferences.getInstance()
+        .then((preferences) => preferences.getInt('washThreshold'));
+  }
+
+  void _selectInWashTimeDialog(BuildContext context) {
+    void _setInWashTime(int value) async {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+      preferences.setInt('washThreshold', value);
+      Navigator.of(context).pop();
+    }
+
+    _getInWashTime().then((inWashTime) => showDialog(
+          context: context,
+          builder: (context) => SimpleDialog(
+            title: Text('Select Time in Wash'),
+            children: List.generate(
+              7,
+              (i) => RadioListTile(
+                title: Text('${i + 1} ${i == 0 ? 'day' : 'days'}'),
+                value: i + 1,
+                groupValue: inWashTime,
+                onChanged: (value) => _setInWashTime(value),
+              ),
             ),
-            RadioListTile(
-              value: Locale('da'),
-              groupValue: language,
-              onChanged: (value) {
-                setState(() {
-                  _select(value);
-                });
-              },
-              title: Text('Dansk'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 
   @override
@@ -84,12 +93,42 @@ class _SettingsScreenState extends State<SettingsScreen> {
       ),
       body: Column(
         children: [
-          RaisedButton(
-              onPressed: () {
-                return selectDialog(context);
-              },
-              child: Text('Language'))
+          _SettingTile(
+            title: 'Language',
+            subtitle: 'Set the Language you want',
+            buttonText: 'Select',
+            onButtonTap: () => _selectLanguageDialog(context),
+          ),
+          _SettingTile(
+            title: 'In Wash Time',
+            subtitle: 'How long is your clothing in wash',
+            buttonText: 'Select',
+            onButtonTap: () => _selectInWashTimeDialog(context),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class _SettingTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String buttonText;
+  final Function onButtonTap;
+  _SettingTile({this.title, this.onButtonTap, this.subtitle, this.buttonText});
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 35, vertical: 10),
+      title: Text(
+        title,
+        style: TextStyle(fontSize: 20),
+      ),
+      subtitle: Text(subtitle),
+      trailing: RaisedButton(
+        onPressed: onButtonTap,
+        child: Text(buttonText),
       ),
     );
   }
