04c1c2c2f41d0d910f13f26e61c051eb4c120959
Alexander Bang <>
2021-01-29 17:14:25 +0100

    updated sql

---
+++ assets/database/data_da.sql
@@ -0,0 +1,3 @@
+INSERT INTO ItemCategories VALUES(1,'Skjorter');
+INSERT INTO ItemCategories VALUES(2,'Bukser');
+INSERT INTO ItemCategories VALUES(3,'Jakkers');
---
+++ assets/database/data_de.sql
@@ -0,0 +1,3 @@
+INSERT INTO ItemCategories VALUES(1,'Hemden');
+INSERT INTO ItemCategories VALUES(2,'Hosen');
+INSERT INTO ItemCategories VALUES(3,'Jacken');
